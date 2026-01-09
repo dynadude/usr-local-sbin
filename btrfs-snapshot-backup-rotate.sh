6eee1446876debc3fd3664969e30388cc5d4faf0
Sagi Gamliel <sagi.gam.11@gmail.com>
2026-01-09 20:34:30 +0200

    btrfs-snapshot-backup-rotate.sh: Standardise variable names

--- btrfs-snapshot-backup-rotate.sh
+++ btrfs-snapshot-backup-rotate.sh
@@ -5,39 +5,39 @@ set -o errexit # abort on nonzero exitstatus
 set -o nounset # abort on unbound variable
 set -o pipefail
 
-SRC_PATH="${1-}"
-SNAPSHOT_PREFIX="${2-}"
-BACKUPS_TO_KEEP="${3-}"
+sourcePath="${1-}"
+snapshotPrefix="${2-}"
+backupsToKeep="${3-}"
 
 # ARGUMENT VALIDATION
-if [ -z "$SRC_PATH" ]; then
+if [ -z "${sourcePath}" ]; then
 	echo 'The source dir to back up was not specified. Exiting...' >&2
 	exit 1
 fi
 
-if ! [ -d "$SRC_PATH" ]; then
+if ! [ -d "${sourcePath}" ]; then
 	echo 'The specified source dir to back up does not exist or is not a directory. Exiting...' >&2
 	exit 2
 fi
 
 # snapshot prefix default value
-if [ -z "$SNAPSHOT_PREFIX" ]; then
+if [ -z "${snapshotPrefix}" ]; then
 	echo 'The snapshot prefix was not specified. Using ".backups/"...'
-	SNAPSHOT_PREFIX='.backups/'
+	snapshotPrefix='.backups/'
 fi
 
 # backups to keep default value
-if [ -z "$BACKUPS_TO_KEEP" ]; then
+if [ -z "${backupsToKeep}" ]; then
 	echo 'The amount of backups to keep was not specified. Using 30...'
-	BACKUPS_TO_KEEP=30
+	backupsToKeep=30
 fi
 
-dirsToRemove=$(find "${SRC_PATH}/${SNAPSHOT_PREFIX}"* -maxdepth 0 | head -n -"${BACKUPS_TO_KEEP}")
+dirsToRemove=$(find "${sourcePath}/${snapshotPrefix}"* -maxdepth 0 | head -n -"${backupsToKeep}")
 
-if [ -n "$dirsToRemove" ]; then
-	echo "Removing Backups:"
-	echo "$dirsToRemove"
-	echo "$dirsToRemove" | xargs btrfs subvolume delete
+if [ -n "${dirsToRemove}" ]; then
+	echo 'Removing Backups:'
+	echo "${dirsToRemove}"
+	echo "${dirsToRemove}" | xargs btrfs subvolume delete
 else
-	echo "No Backups to Remove"
+	echo 'No Backups to Remove'
 fi
