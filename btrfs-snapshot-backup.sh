f586bc977e2d8aaddbbaa5311f3de31f62ad1655
Sagi Gamliel <sagi.gam.11@gmail.com>
2026-01-09 20:26:10 +0200

    btrfs-snapshot-backup.sh: Standardise variable names

--- btrfs-snapshot-backup.sh
+++ btrfs-snapshot-backup.sh
@@ -5,26 +5,26 @@ set -o errexit # abort on nonzero exitstatus
 set -o nounset # abort on unbound variable
 set -o pipefail
 
-SRC_PATH="${1-}"
-SNAPSHOT_PREFIX="${2-}"
+sourcePath="${1-}"
+snapshotPrefix="${2-}"
 
 # ARGUMENT VALIDATION
-if [ -z "$SRC_PATH" ]; then
+if [ -z "${sourcePath}" ]; then
 	echo 'The source dir to back up was not specified. Exiting...' >&2
 	exit 1
 fi
 
-if ! [ -d "$SRC_PATH" ]; then
+if ! [ -d "${sourcePath}" ]; then
 	echo 'The specified source dir to back up does not exist or is not a directory. Exiting...' >&2
 	exit 2
 fi
 
 # snapshot prefix default value
-if [ -z "$SNAPSHOT_PREFIX" ]; then
+if [ -z "${snapshotPrefix}" ]; then
 	echo 'The snapshot prefix was not specified. Using ".backups/"...'
-	SNAPSHOT_PREFIX='.backups/'
+	snapshotPrefix='.backups/'
 fi
 
-TARGET_DIR="${SRC_PATH}/${SNAPSHOT_PREFIX}$(date +"%Y-%m-%d_%H:%M:%S")"
+targetDir="${sourcePath}/${snapshotPrefix}$(date +"%Y-%m-%d_%H:%M:%S")"
 
-btrfs subvolume snapshot -r "${SRC_PATH}" "${TARGET_DIR}"
+btrfs subvolume snapshot -r "${sourcePath}" "${targetDir}"
