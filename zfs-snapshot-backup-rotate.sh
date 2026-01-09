a7c9f7c52799310cbdaf5b2f6ce0dc177dfff2a8
Sagi Gamliel <sagi.gam.11@gmail.com>
2026-01-09 20:15:31 +0200

    zfs-snapshot-backup-rotate.sh: Add backupsToKeep positional parameter, with a default value of 30

--- zfs-snapshot-backup-rotate.sh
+++ zfs-snapshot-backup-rotate.sh
@@ -5,11 +5,15 @@ set -o errexit # abort on nonzero exitstatus
 set -o nounset # abort on unbound variable
 set -o pipefail
 
-backupsToKeep=30
-
 dataset="${1}"
+backupsToKeep="${2-}"
+# backups to keep default value
+if [ -z "$backupsToKeep" ]; then
+	backupsToKeep=30
+	echo "The amount of backups to keep was not specified. Using ${backupsToKeep}..."
+fi
 
-dirsToRemove=$(zfs list -t snapshot -o name "${dataset}" | tail -n +2 | head -n -${backupsToKeep})
+dirsToRemove=$(zfs list -t snapshot -o name "${dataset}" | tail -n +2 | head -n -"${backupsToKeep}")
 
 if [ -n "$dirsToRemove" ]; then
 	echo "Removing Backups:"
