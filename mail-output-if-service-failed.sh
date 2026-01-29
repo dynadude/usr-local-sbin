557d6cce3907fc0f519d108ae129a512c28fd662
Sagi Gamliel <sagi.gam.11@gmail.com>
2026-01-29 23:03:09 +0200

    Add mail-output-if-service-failed.sh

---
+++ mail-output-if-service-failed.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Import functions
+SCRIPT_DIR="$(realpath "$(dirname "${0}")")"
+. "${SCRIPT_DIR}/backup-utils.sh"
+
+# BASH STRICT MODE
+set -o nounset # abort on unbound variable
+set -o pipefail
+
+function getOutputOfLastRunOfService() (
+	serviceName="${1}"
+
+	invocationId="$(systemctl show -p InvocationID --value "${serviceName}")" || return 1
+	if [ -z "${invocationId}" ]; then
+		return 1
+	fi
+
+	journalctl _SYSTEMD_INVOCATION_ID="${invocationId}"
+)
+
+serviceName="${1}"
+emailAddress="${2-sagi.gam.11@gmail.com}"
+
+if (
+	set +o pipefail
+	systemctl show "${serviceName}" | grep '^ActiveState' | grep -i fail >/dev/null
+); then
+	mailContent=$(
+		cat <<-EOF
+			Subject: Service '${serviceName}' Failed
+
+			$(getOutputOfLastRunOfService "${serviceName}" || (
+				echo "Failed at getting the logs of the last run of '${serviceName}'. Aborting..."
+				exit 1
+			))
+		EOF
+	)
+
+	echo "Service '${serviceName}' failed. Attempting to send mail..."
+	echo "${mailContent}" | sendmail "${emailAddress}"
+else
+	echo "The last run of '${serviceName}' was successful. Not sending mail..."
+fi
